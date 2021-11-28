8c150ca9ca4fa1cd54364ed2f1ebffc6759bd5ff
Andrew Pobrica <pirit66@gmail.com>
2021-11-27 19:41:35 -0500

    Added call to moss and a wait for it to return

--- app/main-process/helloWorld.js
+++ app/main-process/helloWorld.js
@@ -6,7 +6,35 @@ export function helloWorld(mainWindowRef, protocolHandler) {
   openHelloWorldWindow(mainWindowRef, protocolHandler)
 }
 
-function openHelloWorldWindow(mainWindow, protocolHandler) {
+async function compare() {
+  // Import packages and ignore any output
+  let {PythonInteractive} = require('python-interactive')
+  let python = new PythonInteractive()
+  await python.start()
+  await python.execute('from interface import *')
+  let userid = 973241060
+  let files = "submission/a01-*.py"
+  let type = "python"
+  // let output = "submission/report/"
+  // Print value of 'pi' and store the output
+
+  // Execute multiline loop command and handle its output via Promise callbacks
+  await python.execute(`remote_call(${userid}, "${files}", "${type}")`)
+      .then((data) => {
+        // If the Python code executed successfully
+        console.log(`webpage stored`);
+      })
+      .catch((err) => {
+        // If the Python code executed with an error
+        console.log(`Failed to execute due to error:\n ${err}`)
+      })
+  await python.stop()
+
+};
+
+
+//console.log(test())
+async function openHelloWorldWindow(mainWindow, protocolHandler) {
   authWindow = new BrowserWindow({
     height: 720,
     width: 1280,
@@ -17,8 +45,10 @@ function openHelloWorldWindow(mainWindow, protocolHandler) {
       nodeIntegration: false,
     },
   })
-
+  await compare()
   const fs = require("fs")
+
+  console.log(process.cwd());
   authWindow.webContents.loadURL("data:text/html;charset=utf-8," + fs.readFileSync("submission/report.html"))
   authWindow.once("ready-to-show", () => {
     if (authWindow) {
